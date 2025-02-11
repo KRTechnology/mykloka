0f5223dbb760d76b2eaed85cd7b9453ed44cce53
Imeh Usoro <imeusoro@rocketmail.com>
2025-02-11 09:11:48 +0100

    fix on middleware block on images

--- middleware.ts
+++ middleware.ts
@@ -67,7 +67,8 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      * - public folder
+     * - images folder (added this)
      */
-    "/((?!_next/static|_next/image|favicon.ico|public/).*)",
+    "/((?!_next/static|_next/image|favicon.ico|public/|images/).*)",
   ],
 };
