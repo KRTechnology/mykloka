7c20021179be633784e166f9a0650feb180c8dbf
Imeh Usoro <imeusoro@rocketmail.com>
2025-02-17 09:11:09 +0100

    fixed metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,12 @@ import { Toaster } from "sonner";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { cn } from "@/lib/utils";
 import "./globals.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Kimberly Ryan",
+  description: "Employee Platform",
+};
 
 export default function RootLayout({
   children,
